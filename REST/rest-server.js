df3cd76d70bb4a63544571cb5ff382bc0e58d299
heiderst16 <stefan.heider@edu.fh-joanneum.at>
2020-04-15 16:05:32 +0200

    added rest example

---
+++ REST/rest-server.js
@@ -0,0 +1,29 @@
+
+/* 
+    Quelle:
+    https://stackabuse.com/building-a-rest-api-with-node-and-express/
+*/
+
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Where we will keep books
+let books = [];
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
